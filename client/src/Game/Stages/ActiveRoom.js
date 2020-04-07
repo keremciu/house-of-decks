58b1855077a0dc6a153804e5ab2dfe61e4f4275b
Kerem Sevencan <kerem@ritmix.org>
2020-04-08 02:19:15 +0300

    put smaller one

--- client/src/Game/Stages/ActiveRoom.js
+++ client/src/Game/Stages/ActiveRoom.js
@@ -57,10 +57,10 @@ function ActiveRoom() {
         }}
       >
         <BlackCard>{blackCard.text}</BlackCard>
-        <div style={{ position: "absolute", bottom: -25, width: "100%" }}>
-          <svg width="1000" height="21" xmlns="http://www.w3.org/2000/svg">
+        <div style={{ position: "absolute", bottom: -54, width: "100%" }}>
+          <svg width="800" height="50" xmlns="http://www.w3.org/2000/svg">
             <path
-              d="M0 .041c56.996 12.607 114.661 19.256 172.996 19.947C260.497 21.025 301.458 6.492 398.13 4.76c96.674-1.733 158.794 6.607 331.231 5.256C844.32 9.113 934.533 5.789 1000 .04H0z"
+              d="M 0 0 Q 120 85 235 10 C 395 20 480 20 665 10 A 100 0 0 0 0 800 0 Z"
               fill="#333"
               fillRule="evenodd"
             />
