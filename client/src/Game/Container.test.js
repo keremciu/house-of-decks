53134ae297329c4941a8e19117551cf278091726
Kerem Sevencan <kerem@ritmix.org>
2020-10-25 21:09:39 +0100

    test env for join room

--- client/src/Game/Container.test.js
+++ client/src/Game/Container.test.js
@@ -13,14 +13,14 @@ test("displays buttons and title", () => {
   screen.getByRole("button", { name: /Join Game/gi });
 });
 
-describe("when user click on start game", () => {
+describe("when user click start game button", () => {
   test("renders create room", () => {
     userEvent.click(screen.getByRole("button", { name: /Start Game/gi }));
     screen.getByLabelText(/Username/gi);
     screen.getByRole("button", { name: /Start a Game/gi });
   });
 
-  test("send create_room request", async () => {
+  test("sends create request and redirects to waiting room", async () => {
     userEvent.click(screen.getByRole("button", { name: /Start Game/gi }));
     userEvent.type(screen.getByLabelText(/Username/gi), "test_user");
     userEvent.click(screen.getByRole("button", { name: /Start a Game/gi }));
@@ -28,7 +28,7 @@ describe("when user click on start game", () => {
   });
 });
 
-describe("when user click on join game", () => {
+describe("when user clicks join game button", () => {
   test("renders join room", async () => {
     render(<Container />);
     await screen.findByText(/house of decks/gi);
@@ -36,5 +36,18 @@ describe("when user click on join game", () => {
     screen.getByLabelText(/Username/gi);
     screen.getByLabelText(/Room ID/gi);
     screen.getByRole("button", { name: /Join the Game/gi });
+  });
+
+  test("sends join request and redirects to waiting room", async () => {
+    render(<Container />);
+    await screen.findByText(/house of decks/gi);
+    userEvent.click(screen.getByRole("button", { name: /Join Game/gi }));
+    userEvent.type(screen.getByLabelText(/Username/gi), "test-user");
+    userEvent.type(screen.getByLabelText(/Room ID/gi), window.gameID);
+    userEvent.click(screen.getByRole("button", { name: /Join the Game/gi }));
+    await screen.findByText(/Click to copy link and share with your friends/gi);
+    screen.getByRole("heading", { name: window.gameID });
+    screen.getByText(/host-user/gi);
+    screen.getByText(/test-user/gi);
   });
 });
--- client/src/utils/server-mock.js
+++ client/src/utils/server-mock.js
@@ -1,24 +1,37 @@
 import { WebSocket, Server } from "mock-socket";
 import RoomService from "../../../server/RoomService";
 import handlers from "../../../server/handlers";
-const fakeURL = "ws://localhost";
+const fakeURL = "ws://localhost:5000";
 const mockServer = new Server(fakeURL);
 
 /*
  * By default the global WebSocket object is stubbed out. However,
  * if you need to stub something else out you can like so:
  */
-
 window.WebSocket = WebSocket; // Here we stub out the window object
+const socket = new window.WebSocket("ws://localhost:5000");
+socket.send(
+  JSON.stringify({
+    action: "create",
+    payload: {
+      username: "host-user",
+    },
+  })
+);
+const onMessage = async (event) => {
+  const data = JSON.parse(event.data);
+  window.gameID = data.game?.id;
+};
+socket?.addEventListener("message", onMessage);
 
 const games = new Map();
 const service = new RoomService(games);
 
 mockServer.on("connection", (ws) => {
-  const broadcastRoom = (game) =>
-    ws.clients.forEach(function each(client) {
+  const broadcastRoom = (game) => {
+    mockServer.clients().forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN && game.id === client.gameID) {
-        client.send(
+        ws.send(
           JSON.stringify({
             game: game.getData(),
             player: game.findPlayer(client.username),
@@ -26,6 +39,7 @@ mockServer.on("connection", (ws) => {
         );
       }
     });
+  };
   ws.send(JSON.stringify({}));
   ws.on("message", (message) => {
     handlers({
